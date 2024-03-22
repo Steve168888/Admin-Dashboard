de204c2ecd5c232de75a700dc29078905457c56e
Steven Sutarjie <stevensutarjie@mimin.io>
2024-03-22 19:41:07 +0700

    push ke github

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -8,8 +8,7 @@ const Layout = ({children}) => {
         <div className={styles.container}>
             <div className={styles.menu}>
                 <Sidebar/>
-            </div>
-      
+            </div> 
             <div className={styles.content}> 
                 <Navbar/>
                 {children}
